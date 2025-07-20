5ecf668e5c7849677e93fe4285c2a1fa4603713b
Gyra Feliz <awwalstudio99@gmail.com>
2025-07-20 23:15:38 +0800

    Add resume download and linkedin button

--- app/about/page.js
+++ app/about/page.js
@@ -5,6 +5,24 @@ export default function About() {
       <p className="text-lg text-gray-600 max-w-xl text-center">
         I’m a passionate developer learning GitHub, Vercel, Next.js, and Tailwind to build awesome things on the web!
       </p>
+
+      <a
+  href="/Awwal Mukhzar Resume.pdf"
+  download
+  className="inline-block bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
+>
+  📄 Download Resume
+</a>
+
+<a
+  href="https://www.linkedin.com/in/awwal-mukhzar-1b5231192/"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="inline-block bg-gray-800 text-white px-6 py-2 rounded hover:bg-gray-900 transition ml-4"
+>
+  🔗 Connect on LinkedIn
+</a>
+
     </main>
   )
 }
