62e4876e644f635e8512716a28a0aa0fdd720957
Gyra Feliz <awwalstudio99@gmail.com>
2025-07-20 23:28:56 +0800

    Update Description about

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ export default function About() {
     <main className="min-h-screen flex flex-col items-center justify-center bg-white">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">About Me</h1>
       <p className="text-lg text-gray-600 max-w-xl text-center">
-        I’m a passionate developer learning GitHub, Vercel, Next.js, and Tailwind to build awesome things on the web!
+        I’m a passionate devOps Engineer learning Cloud Infrastructure,GitHub, Vercel, Next.js, and Tailwind to build awesome things on the web!
       </p>
 
       <a
